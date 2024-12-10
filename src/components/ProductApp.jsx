7869925ed56ae653ec8acf27c96175ffb7f21ba8
JulioHerreraSe <julio.herreraseara@gmail.com>
2024-12-10 18:43:57 +0100

    Initial commit

---
+++ src/components/ProductApp.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { create, findAll, remove, update } from "../services/ProductService";
+import { ProductGrid } from "./ProductGrid";
+import PropTypes from "prop-types";
+import { ProductForm } from "./ProductForm";
+
+export const ProductApp = ( {title} ) => {
+
+    const [products, setProducts] = useState([]);
+
+    const [productSelected, setProductSelected] = useState({
+        id: 0,
+        name: '',
+        description: '',
+        price: ''
+    })
+
+    const getProducts = async () => {
+        const result = await findAll();
+        setProducts(result.data._embedded.products)
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const handlerAppProduct = async (product) => {
+        console.log(product)
+
+        if(product.id > 0) {
+            const response = await update(product);
+            setProducts(products.map(prod => {
+                if(prod.id == product.id){
+                    return {...response.data}
+                }
+                return prod;
+            }));
+        } else {
+            const response = await create(product);
+            setProducts([...products, {...response.data}])
+        }
+    }
+
+    const handlerRemoveProduct = (id) => {
+        //console.log(id);
+        remove(id);
+        setProducts(products.filter(product => product.id != id));
+    }
+
+    const handlerProductSelected = (product) => {
+        setProductSelected({...product});
+    }
+
+    return (
+        <div className="container my-4">
+            <div className="row">
+                <div className="col">
+                    <ProductForm handlerAdd = { handlerAppProduct } productSelected= { productSelected } />
+                </div>
+                <div className="col">
+                <h2>{ title }</h2>
+                    {
+                        products.length > 0 ? <ProductGrid products={ products } handlerRemove = { handlerRemoveProduct } handlerProductSelected={handlerProductSelected} />
+                            : <div className="alert alert-warning">No hay productos en el sistema!</div> 
+                    }
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+ProductApp.propType = {
+    title: PropTypes.string.isRequired
+}
